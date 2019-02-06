1d1747e36970325740cfc5d1e0d4b7eaf2d85074
TaeOtut <johnk.sison@gmail.com>
2019-02-06 10:43:40 -0500

    local storage

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 // Init weather object
-const weather = new Weather('Boston', 'United States');
 const ui = new UI;
+const storage = new Storage;
+const weatherLocation = storage.getLocationData();
 
-//document.addEventListener('DOMContentLoader', getWeather);
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 
+document.addEventListener('DOMContentLoaded', getWeather);
 
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+   const city = document.getElementById('city').value;
+   const country = document.getElementById('country').value;
 
-//function getWeather() {
+   weather.changeLocation(city, country);
+   storage.setLocationData(city, country);
+
+   getWeather();
+
+   $('#locModal').modal('hide');
+})
+
+function getWeather() {
    weather.getWeather()
       .then(results => {
-         ui.paint(results);
          console.log(results);
+         ui.paint(results);
       })
       .catch(err => {
          console.log(err);
       });
-//}
+}
