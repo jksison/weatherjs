6e94c3e301b9315dd526a69d31dbbf9fefce94bf
trma004 <trma004@TenderRetail-02s-MacBook-Air.local>
2019-02-06 10:19:48 -0500

    added get weather logic and ui

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 // Init weather object
-const weather = new Weather('London', 'uk');
+const weather = new Weather('Boston', 'United States');
+const ui = new UI;
 
-document.addEventListener('DOMContentLoader', getWeather);
+//document.addEventListener('DOMContentLoader', getWeather);
 
 
 
-function getWeather() {
+//function getWeather() {
    weather.getWeather()
       .then(results => {
+         ui.paint(results);
          console.log(results);
       })
       .catch(err => {
          console.log(err);
       });
-}
+//}
